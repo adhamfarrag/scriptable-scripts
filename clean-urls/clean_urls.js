af86175e80eec6e179c44e65d106fc2f017a96aa
Adham Farrag <adham.farrag@hotmail.com>
2023-04-01 01:23:02 +0800

    👌 IMPROVE: Handing all cases

--- clean-urls/clean_urls.js
+++ clean-urls/clean_urls.js
@@ -4,22 +4,33 @@ if (typeof url !== 'string') {
   url = url.toString()
 }
 
-if (url.length > 0 && url.includes('?')) {
-  url = url.split('?')[0]
-} else {
+if (!isValidURL(url)) {
   let notification = new Notification()
+
   notification.title = 'No URL Found'
   notification.body = 'Please, add a URL'
   notification.schedule('no-url-found-notification')
+} else if (url.length > 0 && !url.includes('?')) {
+  let notification = new Notification()
+
+  notification.title = 'Already clean'
+  notification.body = 'The URL is already clean'
+  notification.schedule('already-clean-notification')
+} else if (url.length > 0 && url.includes('?')) {
+  url = url.split('?')[0]
+
+  let notification = new Notification()
+
+  notification.title = 'URL Cleaned'
+  notification.body = 'The URL has been cleaned and copied to the clipboard'
+  notification.schedule('cleaned-url-notification')
+
+  Pasteboard.copyString(url)
+  ShareSheet.present([url])
 }
 
-Pasteboard.copyString(url)
-
-ShareSheet.present([url])
-
-let notification = new Notification()
-notification.title = 'URL Cleaned'
-notification.body = url
-notification.schedule('cleaned-url-notification')
-
-url
+function isValidURL(url) {
+  let regex =
+    /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(#[-a-z\d_]*)?$/i
+  return regex.test(url)
+}
